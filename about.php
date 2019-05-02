ea5d5c55308bb34cf4b3ea433e42845a648cd9b2
Jason Ye <yeja@oregonstate.edu>
2019-05-02 15:18:49 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php $pagetitle="Welcome"; ?>
+<?php include 'header.htm'; ?>
+
+<h2>Random text</h2>
+		<p>
+				Continuing melancholy especially so to. Me unpleasing impossible in attachment announcing so astonished. What ask leaf may nor upon door. Tended remain my do stairs. Oh smiling amiable am so visited cordial in offices hearted. 
+				
+				Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh. Hundred no prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding ask expense are compact. New all paid few hard pure she. 
+				
+				Add you viewing ten equally believe put. Separate families my on drawings do oh offended strictly elegance. Perceive jointure be mistress by jennings properly. An admiration at he discovered difficulty continuing. We in building removing possible suitable friendly on. Nay middleton him admitting consulted and behaviour son household. Recurred advanced he oh together entrance speedily suitable. Ready tried gay state fat could boy its among shall. 
+				
+				Offices parties lasting outward nothing age few resolve. Impression to discretion understood to we interested he excellence. Him remarkably use projection collecting. Going about eat forty world has round miles. Attention affection at my preferred offending shameless me if agreeable. Life lain held calm and true neat she. Much feet each so went no from. Truth began maids linen an mr to after. 
+				
+
+		</p>
+		<section id="about" class="mz-module">
+				<div class="container light-bg">
+					<div class="row">
+						<div class="col-lg-12 text-center">
+							<div class="section-title">
+								<h2>ABOUT</h2>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="text-center">
+							<div class="mz-about-container">
+								<p>A creative agency based on Candy Land, ready to boost your business with some beautifull templates. Lattes Agency is one of the best in town see more you will be amazed.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+		</section>
